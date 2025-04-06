3106cc6a03b57e5dd6bb819eb8addb2cd3b5af0a
Isaac Connor <isaac@zoneminder.com>
2025-04-05 21:30:03 -0400

    Add EndComment.sh

---
+++ examples/EventEndCommand.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+trap 'cleanup' SIGINT SIGTERM
+
+cleanup() {
+  exit 1
+}
+
+if [[ -z "$1" ]]; then
+  echo "An event ID MUST be passed as ARG 1, Monitor ID for ARG 2 is optional"
+  exit 1
+  else
+    EID="--eid $1"
+fi
+if [[ -n "$2" ]]; then
+  MID="--mid $2"
+  else
+    MID=""
+fi
+echo "ML_CLIENT_CONF_FILE: ${ML_CLIENT_CONF_FILE}"
+config="${ML_CLIENT_CONF_FILE:-/opt/zomi/client/conf/client.yml}"
+detect_script="${ML_CLIENT_EVENT_START:-$(which zomi-eventproc)}"
+
+event_start_command=(
+#  python3
+  "${detect_script}"
+  --config "${config}"
+  --event-mode
+  "${EID}"
+  "${MID}"
+)
+
+echo "Running command: ${event_start_command[*]}"
+
+eval "${event_start_command[@]}"
+exit 0
